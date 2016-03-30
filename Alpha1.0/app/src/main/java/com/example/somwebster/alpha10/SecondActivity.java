6c52d033178150f2be909f8836a71febb7bb0c8d
Som webster <somnathfreak007@gmail.com>
2016-03-30 21:43:17 +0530

    Beginner android app
    Basic app for android developers

---
+++ Alpha1.0/app/src/main/java/com/example/somwebster/alpha10/SecondActivity.java
@@ -0,0 +1,17 @@
+package com.example.somwebster.alpha10;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by somwebster on 05/02/16.
+ */
+public class SecondActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+
+    }
+}
---
+++ Alpha1.0/app/src/main/java/com/example/somwebster/alpha10/ThirdActivity.java
@@ -0,0 +1,16 @@
+package com.example.somwebster.alpha10;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by somwebster on 06/02/16.
+ */
+public class ThirdActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.third_layout);
+
+    }
+}
